fb24ddc2bfc2d5c2a8bd2e474950dd4ef064dee5
Lorik Limani <limani.lorik@ibu.edu.mk>
2026-02-06 01:39:48 +0100

    Implement user input and display for student info
    Added user input prompts and display for student information.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // TODO: Declare variables
+    string fullName;
+    int age;
+    string studyProgram;
+
+    // TODO: Ask user for input
+    cout << "Enter your full name: ";
+    getline(cin, fullName);
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cin.ignore(); // Ignore leftover newline
+
+    cout << "Enter your study program: ";
+    getline(cin, studyProgram);
+
+    // TODO: Display the information
+    cout << "\nStudent Information\n";
+    cout << "-------------------\n";
+    cout << "Name: " << fullName << endl;
+    cout << "Age: " << age << endl;
+    cout << "Program: " << studyProgram << endl;
+
+    return 0;
+}
 
